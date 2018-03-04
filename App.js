977b47cc7e47b85147c34b0226a2b0d6d5049753
Wellington Oliveira <well.oliveira.snt@gmail.com>
2018-03-04 19:31:26 -0400

    Initial

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ListOfItems from "./components/ListOfItems/ListOfItems";
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <ListOfItems/>
+        );
+    }
+}
---
+++ utils/firebase.js
@@ -0,0 +1,29 @@
+import firebase from 'firebase';
+
+const prodConfig = {
+    apiKey: "***************",
+    authDomain: "***************",
+    databaseURL: "***************",
+    projectId: "***************",
+    storageBucket: "***************",
+    messagingSenderId: "***************"
+};
+
+const devConfig = {
+    apiKey: "***************",
+    authDomain: "***************",
+    databaseURL: "***************",
+    projectId: "***************",
+    storageBucket: "***************",
+    messagingSenderId: "***************"
+};
+
+const config = process.env.NODE_ENV === 'production'
+    ? prodConfig
+    : devConfig;
+
+export const firebaseImpl = firebase.initializeApp(config);
+export const firebaseDatabase = firebase.database();
+export const firebaseAuth = firebase.auth();
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
+export const facebookProvider = new firebase.auth.FacebookAuthProvider();
